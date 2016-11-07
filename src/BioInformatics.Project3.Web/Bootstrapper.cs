256a01c522dfb20f3665050d995114bb0a58cccc
michal.niegrzybowski@gmail.com <michal.niegrzybowski@gmail.com>
2016-11-07 22:57:32 +0100

    fix references in csproj

--- src/BioInformatics.Project3.Web/Bootstrapper.cs
+++ src/BioInformatics.Project3.Web/Bootstrapper.cs
@@ -1,9 +1,15 @@
-﻿using Nancy;
+﻿using Autofac;
+using Nancy.Bootstrappers.Autofac;
 
 namespace BioInformatics.Project3.Web
 {
-    public class Bootstrapper : DefaultNancyBootstrapper
+    public class Bootstrapper : AutofacNancyBootstrapper
     {
+        protected override void ConfigureApplicationContainer(ILifetimeScope existingContainer)
+        {
+            base.ConfigureApplicationContainer(existingContainer);
+        }
+
         // The bootstrapper enables you to reconfigure the composition of the framework,
         // by overriding the various methods and properties.
         // For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
