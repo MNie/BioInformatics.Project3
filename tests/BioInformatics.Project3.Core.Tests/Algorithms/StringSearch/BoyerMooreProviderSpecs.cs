6f962f9aa65829be2ffe9f099f5ecea0a7577697
michal.niegrzybowski@gmail.com <michal.niegrzybowski@gmail.com>
2016-12-12 19:54:30 +0100

    rename variables in spec

--- tests/BioInformatics.Project3.Core.Tests/Algorithms/StringSearch/BoyerMooreProviderSpecs.cs
+++ tests/BioInformatics.Project3.Core.Tests/Algorithms/StringSearch/BoyerMooreProviderSpecs.cs
@@ -12,20 +12,20 @@ namespace BioInformatics.Project3.Core.Tests.Algorithms.StringSearch
 {
     public class BoyerMooreProviderSpecs
     {
-        private static Sequence seq;
-        private readonly IBoyerMooreProvider sut;
+        private static Sequence _seq;
+        private readonly IBoyerMooreProvider _sut;
         public BoyerMooreProviderSpecs()
         {
-            seq = new Sequence(DnaAlphabet.Instance.GetMummerAlphabet(), "GGCCC");
+            _seq = new Sequence(DnaAlphabet.Instance.GetMummerAlphabet(), "GGCCC");
             var sequenceProvider = MockRepository.GenerateStub<ISequenceProvider>();
-            sequenceProvider.Stub(x => x.Provide(null, null)).IgnoreArguments().Return(new[] { seq });
-            sut = new BoyerMooreProvider(sequenceProvider, new BoyerMoore());
+            sequenceProvider.Stub(x => x.Provide(null, null)).IgnoreArguments().Return(new[] { _seq });
+            _sut = new BoyerMooreProvider(sequenceProvider, new BoyerMoore());
         }
 
         [Fact]
         public void Should_return_proper_count_of_matches()
         {
-            var result = sut.GetMatches(null, new [] {"G", "CC", "GG", "cc"});
+            var result = _sut.GetMatches(null, new [] {"G", "CC", "GG", "cc"});
             result["G"].Count.ShouldBe(2);
             result["CC"].Count.ShouldBe(2);
             result["cc"].Count.ShouldBe(0);
@@ -35,7 +35,7 @@ namespace BioInformatics.Project3.Core.Tests.Algorithms.StringSearch
         [Fact]
         public void Should_return_proper_count_of_matches_when_we_ignore_case()
         {
-            var result = sut.GetMatches(null, new[] { "G", "cc" }, true);
+            var result = _sut.GetMatches(null, new[] { "G", "cc" }, true);
             result["G"].Count.ShouldBe(2);
             result["cc"].Count.ShouldBe(2);
         }
@@ -43,7 +43,7 @@ namespace BioInformatics.Project3.Core.Tests.Algorithms.StringSearch
         [Fact]
         public void Should_return_proper_count_of_matches_when_we_start_from_custom_index()
         {
-            var result = sut.GetMatches(null, new[] { "G", "CC" }, startIndex: 3);
+            var result = _sut.GetMatches(null, new[] { "G", "CC" }, startIndex: 3);
             result["G"].Count.ShouldBe(0);
             result["CC"].Count.ShouldBe(1);
         }
