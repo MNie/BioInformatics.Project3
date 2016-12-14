3b196c85fb0616e204236425a6da46dd76d8246a
michal.niegrzybowski@gmail.com <michal.niegrzybowski@gmail.com>
2016-12-14 18:34:05 +0100

    fix problem with passing parameters to method

--- src/BioInformatics.Project3.Core/Providers/SequenceProvider.cs
+++ src/BioInformatics.Project3.Core/Providers/SequenceProvider.cs
@@ -17,6 +17,7 @@ namespace BioInformatics.Project3.Core.Providers
     {
         public IReadOnlyCollection<ISequence> Provide(string fileName, string content)
         {
+            content = content.Replace(@"\n", "\n");
             IReadOnlyCollection<ISequence> sequences = new ISequence[0];
             var byteArray = Encoding.UTF8.GetBytes(content);
             MemoryStream stream = null;
