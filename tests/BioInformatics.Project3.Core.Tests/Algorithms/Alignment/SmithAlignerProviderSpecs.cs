2c31678b5abfda8121c59ddf60b8c129e5fae8cd
michal.niegrzybowski@gmail.com <michal.niegrzybowski@gmail.com>
2016-12-21 21:48:44 +0100

    fix specs

--- tests/BioInformatics.Project3.Core.Tests/Algorithms/Alignment/SmithAlignerProviderSpecs.cs
+++ tests/BioInformatics.Project3.Core.Tests/Algorithms/Alignment/SmithAlignerProviderSpecs.cs
@@ -17,8 +17,12 @@ namespace BioInformatics.Project3.Core.Tests.Algorithms.Alignment
         private readonly SmithAlignerProvider _sut;
         public SmithAlignerProviderSpecs()
         {
-            _seq1 = new Sequence(Alphabets.DNA, "GG");
-            _seq2 = new Sequence(Alphabets.DNA, "AACC");
+            _seq1 = new Sequence(Alphabets.DNA, "AGCGTAG");
+            _seq2 = new Sequence(Alphabets.DNA, "CTCGTC");
+            //_seq1 = new Sequence(Alphabets.DNA, "GACTTAC");
+            //_seq2 = new Sequence(Alphabets.DNA, "CGTGAATTCAT");
+            //_seq1 = new Sequence(Alphabets.DNA, "AACGTTGAC");
+            //_seq2 = new Sequence(Alphabets.DNA, "AAGGTATGAATC");
             var sequenceProvider = MockRepository.GenerateStub<ISequenceProvider>();
             sequenceProvider.Stub(x => x.Provide(null, null)).IgnoreArguments().Return(new[] { _seq1, _seq2 });
             _sut = new SmithAlignerProvider(sequenceProvider, new SmithWatermanAligner());
@@ -28,14 +32,16 @@ namespace BioInformatics.Project3.Core.Tests.Algorithms.Alignment
         public void should_return_proper_sequences_for_align()
         {
             var result = _sut.AlignSequences(new[] { new SequenceModel() });
-            result.First().ToString().Trim().ShouldBe("RRCC\r\nGG--\r\nAACC");
+            result.First().ToString().Trim().ShouldBe("CGT\r\nCGT\r\nCGT");
+            //result.First().ToString().Trim().ShouldBe("GAMTT\r\nGACTT\r\nGAATT");
+            //result.First().ToString().Trim().ShouldBe("AASGT\r\nAACGT\r\nAAGGT");
         }
 
         [Fact]
         public void should_return_proper_sequences_for_simple_align()
         {
             var result = _sut.AlignSequencesSimple(new[] { new SequenceModel() });
-            result.First().ToString().Trim().ShouldBe("RRCC\r\nGG--\r\nAACC");
+            result.First().ToString().Trim().ShouldBe("CGT\r\nCGT\r\nCGT");
         }
 
         ~SmithAlignerProviderSpecs()
